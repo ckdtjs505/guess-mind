4381f89edc3a8f4137d37648a1f7c10bae53e093
ckdtjs505 <ckdtjs505@naver.com>
2020-02-11 22:17:04 +0900

    Feat : server setting ( pug, static, express)

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
 import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "\\src\\views");
+
+app.use(express.static(process.cwd() + "\\src\\static"));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(port, () =>
+  console.log("✅  Server running: http://localhost:4000")
+);
